f8bce220d7bcf8ba0a4f55d5b89720b50eb49807
rainashastri30 <rainagit30>
2015-01-09 14:33:20 -0800

    Removing duplicates

--- Yougurtland Runner/ScoreSubject.java
+++
@@ -1,8 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public interface ScoreSubject
-{
-    public void attach(ScoreObserver obj);
-    public void detach(ScoreObserver obj);
-    public void notifyObservers(String scoreElement);
-}
